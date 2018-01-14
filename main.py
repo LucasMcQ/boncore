fd057297fd0960804898d26fd0095747094cbcc1
lucasmcq <lmcq12@email.arizona.edu>
2018-01-14 08:43:21 -0700

    hardware files

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import time
+import pyb
 from pyb import UART
 
 uart = UART(6, 9600)
 uart.init(9600, bits=8, parity=None, stop=1)
 
 filename = "gpsData.txt"
-file = open(filename, "w")
+uart.write("$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*2C\r\n")
 
-#for i in range(5):
-line = uart.readline()
-file.write(line)    
+i = 0
+while i < 30:
+    if uart.any() > 0:
+        file = open(filename, "a")
+        line = uart.readline()
+        line = line.decode("utf-8") #ser.readline returns a binary, convert to string
+        file.write(line)
+        file.flush()
+        i = i + 1
+        file.close()
 
-file.close()
+pyb.LED(1).on()
+pyb.LED(2).on()
+pyb.LED(3).on()
+pyb.LED(4).on()
